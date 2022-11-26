e4b4010009e66a2a43215dc842ce0583397f64c4
wibyweb <49052850+wibyweb@users.noreply.github.com>
2022-11-26 01:31:02 -0500

    Update form.html.php

--- html/submit/form.html.php
+++ html/submit/form.html.php
@@ -40,26 +40,15 @@
 	  <div>
 	    <input type="text" name="captcha_code" size="10" maxlength="6" />
 	    <a href="#" onclick="document.getElementById('captcha').src = '/securimage/securimage_show.php?' + Math.random(); return false">Reload Image</a>
-      </div>  
+      </div>
+      <p class="pin">* Cookies must be enabled for the captcha.</p>	    
       <?php endif; ?>
       <br>
       <div><input type="submit" value="Submit"/></div>    
       <br><br>
       <h3>What kind of pages get indexed?</h3>
       <p>
-        Pages must be simple in design. Simple HTML, <b>non-commerical</b> sites are preferred.<br> 
-        <b>Pages should not use much scripts/css for cosmetic effect.</b> Some might squeak through.<br>
-	    Don't use ads that are intrusive (such as ads that appear overtop of content).<br>
-	    Don't submit a page which serves primarily as a portal to other bloated websites.<br>
-	    If you submit a blog, submit a few of your articles, not your main feed.<br>
-	    If your page does not contain any text or uses frames, ensure a meta description tag is added.<br>
-	    Only the page you submit will be crawled.<br>
-      </p>
-      <p class="pin">
-        <br><br>Note:<br>
-	<br>The WibyBot (172.93.49.59) is occasionally rejected by some web servers. 
-	<br>Barring technical issues, if you are puzzled why a site wasn't indexed, reread the above guide.
-	<br>Angelfire and Tripod pages are no longer grandfathered (ads are too intrusive).
+	      Enter your submission rules here.
       </p>
     </form>    
 
