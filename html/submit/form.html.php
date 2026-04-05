cb6e5009fc01207953955a12e75721cea437a64b
wibyweb <49052850+wibyweb@users.noreply.github.com>
2026-04-05 14:13:06 -0400

    Add files via upload

--- html/submit/form.html.php
+++ html/submit/form.html.php
@@ -4,40 +4,40 @@
 
   <head>    
     <title>Submit to the Wiby Web</title>   
-    <meta http-equiv="content-type" content="text/html; charset=utf-8"/> 
-    <meta name="viewport" content="width=device-width, initial-scale=1">  
-    <LINK REL=STYLESHEET HREF="../styles.css" TYPE="text/css">  
-    <style type="text/css">    
-      textarea {   
-        display: block;   
-        width: 100%;   
-      }   
-    </style>  
-  </head>    
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <LINK REL=STYLESHEET HREF="../styles.css" TYPE="text/css">
+    <style type="text/css">
+      textarea {
+        display: block;
+        width: 100%; 
+      }
+    </style>
+  </head>   
 
-  <body>    
-    <form method="post"> 
+  <body>
+    <form method="post">
 
       <div>
 
-        <label for="url">url:</label>   
+        <label for="url">url:</label>
 	<?php if(isset($_POST['url'])) : ?>
             <input type="text" id="url" name="url" value="<?php echo htmlspecialchars($_POST['url'], ENT_QUOTES, 'UTF-8') ?>" maxlength="400" >
 	<?php else : ?>
 	    <input type="text" id="url" name="url" maxlength="400" >
 	<?php endif; ?>
 	
-      </div>    
+      </div>
 
       <div>
         <label for="worksafe">worksafe:</label>
-	<?php if(isset($_POST['url']) && $_POST['worksafe'] != 'on') : ?>   
+	<?php if(isset($_POST['url']) && $_POST['worksafe'] != 'on') : ?>
           <input type="checkbox" id="worksafe" name="worksafe">
 	<?php else : ?>
 	  <input type="checkbox" id="worksafe" name="worksafe" checked="checked">
 	<?php endif; ?>
-      </div><br> 
-      <?php if($_SESSION["authenticated"] != true): ?> 
+      </div><br>
+      <?php if($_SESSION["authenticated"] != true): ?>
 	  <div>
 	    <img id="captcha" src="/securimage/securimage_show.php" alt="CAPTCHA Image" />
 	  </div>
@@ -52,19 +52,7 @@
       <br><br>
       <h3>What kind of pages get indexed?</h3>
       <p>
-        Pages must be simple in design. Simple HTML, <b>non-commerical</b> sites are preferred.<br> 
-        <b>Pages should not use much scripts/css for cosmetic effect.</b> Some might squeak through.<br>
-	    Don't use ads that are intrusive (such as ads that appear overtop of content).<br>
-	    Don't submit a page which serves primarily as a portal to other bloated websites.<br>
-	    Avoid submitting multiple tens of pages from one person's website.<br>
-	    If your page does not contain any text or uses frames, ensure a meta description tag is added.<br>
-	    In most cases, only the page you submit will be crawled.<br>
-      </p>
-      <p class="pin">
-        <br><br>Note:<br>
-	<br>The WibyBot (172.93.49.252) is occasionally rejected by some web servers. 
-	<br>Barring technical issues, if you are puzzled why a site wasn't indexed, reread the above guide.
-	<br>Angelfire and Tripod pages are no longer grandfathered (ads are too intrusive).
+	      Enter your submission rules here.
       </p>
     </form>    
 
